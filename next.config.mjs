204cc0b0077a963a266f76ab5bb5aa6210d7dd90
Vighnesh Kumar <vighneshkumariit@gmail.com>
2024-10-19 18:12:09 +0530

    changes for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
-// Uncomment below for local development and comment for GitHub deployment (npm run deploy)
+
+// Check if the app is running in GitHub Actions environment
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // Get the repository name from the environment variable
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
-  output: "export"
+  output: "export",  // Required for static export (used by GitHub Pages)
+  assetPrefix: assetPrefix,  // Prefix for static assets
+  basePath: basePath,  // Base path for routing
+  images: {
+    unoptimized: true,  // Disable image optimization for GitHub Pages
+  },
 };
 
 export default nextConfig;
-
-
-// Comment below for local development and uncomment for deploying this static app to GitHub pages
-// export default {
-//   output: "export", // Enable static export
-//   basePath: "/my-portfolio", // Add the name of your repository
-//   images: {
-//     unoptimized: true, // Disable image optimization for GitHub Pages
-//   },
-//   trailingSlash: true,
-// };
